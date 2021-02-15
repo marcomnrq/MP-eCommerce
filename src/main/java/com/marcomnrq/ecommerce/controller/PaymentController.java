2e715898b3acfba25e340c90d8ffc31c6c5eec29
Marco Antonio Manrique Acha <63822742+MarcoMnrq@users.noreply.github.com>
2021-02-15 18:27:55 -0500

    Update PaymentController.java

--- src/main/java/com/marcomnrq/ecommerce/controller/PaymentController.java
+++ src/main/java/com/marcomnrq/ecommerce/controller/PaymentController.java
@@ -1,8 +1,11 @@
 package com.marcomnrq.ecommerce.controller;
 
+import com.marcomnrq.ecommerce.domain.model.Payment;
 import com.marcomnrq.ecommerce.resource.MercadopagoResource;
 import com.marcomnrq.ecommerce.service.PaymentService;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,5 +27,10 @@ public class PaymentController {
     public ResponseEntity paymentNotification(@RequestParam String parameters, @RequestBody String body){
         paymentService.paymentNotification(parameters, body);
         return ResponseEntity.ok(200);
+    }
+
+    @GetMapping
+    public Page<Payment> getAllPayments(Pageable pageable){
+        return paymentService.getAllPayments(pageable);
     }
 }
